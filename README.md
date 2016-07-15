5e168597630cc58668bf5ab2537e2eda6957a839
Salvador Fuentes Jr <fuentesjr@gmail.com>
2016-07-14 23:40:55 -0700

    Seeding commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Elm Webpack Project Template 
+This serves as the base template I use for creating new Elm projects. Feel free to tweak and use it
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [
+      './src/index.js'
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!html!src/index.html'
+    })
+  ],
+
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: '[name].[hash].js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test:    /\.html$/,
+        exclude: /node_modules/,
+        loader:  'file?name=[name].[ext]',
+      },
+      {
+        test:    /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader:  'elm-webpack',
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+    ],
+
+    noParse: /\.elm$/,
+  },
+
+  devServer: {
+    inline: true,
+    stats: { colors: true },
+  },
+
+};
